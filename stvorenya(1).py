4d96a976aaa70adf8e4a46b4a7e3917fa5767ef6
hastekk-ppk <114733030+hastekk-ppk@users.noreply.github.com>
2022-10-07 10:39:16 +0300

    Delete stvorenya(1).py

--- stvorenya(1).py
+++
@@ -1,6 +0,0 @@
-first = int(input('Vvedite pervoe chislo: '))
-second = int(input('Vvedite vtoroe chislo: '))
-print('Syma: ', first + second);
-print('Riznitsya: ', first - second);
-print('Dobytok: ', first * second);
-print('Chastka: ', first / second);
